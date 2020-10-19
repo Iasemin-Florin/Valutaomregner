457d83c45f3507e09c0159c6565a8f4ef723995d
Iasemin-Florin <queue.triple@gmail.com>
2020-10-19 12:16:36 +0200

    UX has been finished, All logic except for the API logic has been written

--- Valutaomregner/Valutaomregner/MainWindow.xaml.cs
+++ Valutaomregner/Valutaomregner/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Valutaomregner
 {
@@ -23,21 +25,108 @@ namespace Valutaomregner
         public MainWindow()
         {
             InitializeComponent();
+            BindCurrency();
         }
 
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+            //Adding columns
+            dtCurrency.Columns.Add("Text");
+            dtCurrency.Columns.Add("Value");
+
+            //Adding rows
+            dtCurrency.Rows.Add("--SELECT--", 0);
+            dtCurrency.Rows.Add("DKK", 0);
+            dtCurrency.Rows.Add("EUR", 744.29);
+            dtCurrency.Rows.Add("USD", 636.25);
+            dtCurrency.Rows.Add("GBP", 823.49);
+            dtCurrency.Rows.Add("SEK", 71.72);
+            dtCurrency.Rows.Add("NOK", 67.86);
+            dtCurrency.Rows.Add("CHF", 695.79);
+            dtCurrency.Rows.Add("JPY", 6.0438);
+
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.SelectedValuePath = "Value";
+            cmbFromCurrency.SelectedIndex = 0;
+
+            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
+
+        }
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
+            double ConvertedValue;
+           
+            //Checking if the currency textbox is empty
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                //Error message
+                MessageBox.Show("Please enter a value: ", "Information", MessageBoxButton.OK);
+                txtCurrency.Focus();
+                return;
+            }
+            //Checking if the From currency combobox has a value that isn't default
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                //Error message
+                MessageBox.Show("Pleae enter what currency you're converting from", "Information", MessageBoxButton.OK);
+                cmbFromCurrency.Focus();
+                return;
+            }
+            //Checking if the To currency combobox has a value that isn't default
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                //Error message
+                MessageBox.Show("Please enter to what currency you're converting to", "Information", MessageBoxButton.OK);
+                cmbToCurrency.Focus();
+                return;
+            }
+            if (cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                //Here we convert the value which is a string, to a double
+                ConvertedValue = double.Parse(txtCurrency.Text);
+                //Show the label converted currency and the converted currency's name the N3 is added to add 3 zero's after the .
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+            else
+            {
+                //Conversion formula
+                ConvertedValue = (double.Parse(cmbFromCurrency.SelectedValue.ToString()) *
+                    double.Parse(txtCurrency.Text)) /
+                    double.Parse(cmbToCurrency.SelectedValue.ToString());
 
+                //Displaying the end result
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
         }
+
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-
+            Clear_Controls();
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-
+            //Using regex to see if there are only numbers in the textbox
+            Regex regex = new Regex("[^0-9]");
+            //Checking if its a match   
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        
+        private void Clear_Controls()
+        {
+            txtCurrency.Text = string.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+                cmbFromCurrency.SelectedIndex = 0;
+            if (cmbToCurrency.Items.Count > 0)
+                cmbToCurrency.SelectedIndex = 0;
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
         }
     }
 }
